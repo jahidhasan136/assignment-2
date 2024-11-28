021cab88a2dbd71dff9863a9d07b9d364fa0024c
jahidhasan136 <jh3495689@gmail.com>
2024-11-28 15:53:36 +0600

    product model section validate

--- src/modules/product/product.model.ts
+++ src/modules/product/product.model.ts
@@ -4,15 +4,20 @@ import { IProduct } from './product.interface';
 const productSchema = new Schema<IProduct>({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Name is required'],
+    maxlength: [10, 'Name can not be more than 10 characters'],
   },
   brand: {
     type: String,
-    required: true,
+    required: [true, 'Brand is required'],
   },
   price: {
     type: Number,
-    required: true,
+    required: [true, 'Price is required'],
+    validate: {
+      validator: (value: number) => value > 0,
+      message: `{VALUE}, price must be greater than 0`,
+    },
   },
   category: {
     type: String,
@@ -24,15 +29,21 @@ const productSchema = new Schema<IProduct>({
       'Educational',
       'Technology',
     ],
-    required: true,
+    required: [true, 'Category not matched the providing requirements'],
   },
   description: {
     type: String,
-    required: true,
+    required: [true, 'Description is required'],
+    minlength: [10, 'Description can not be less than 10 character'],
+    maxlength: [50, 'Description can not be more than 50 character'],
   },
   quantity: {
     type: Number,
-    required: true,
+    required: [true, 'Quantity is required'],
+    validate: {
+      validator: (value: number) => value > 0,
+      message: `{VALUE}, quantity must be greater than 0`,
+    },
   },
   inStock: {
     type: Boolean,
