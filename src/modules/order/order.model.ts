1334efcc0f6cf33aca130112246b956043697cd1
jahidhasan136 <jh3495689@gmail.com>
2024-11-28 16:57:02 +0600

    orders validation successfully

--- src/modules/order/order.model.ts
+++ src/modules/order/order.model.ts
@@ -1,12 +1,20 @@
-import mongoose, { model, ObjectId, Schema } from 'mongoose';
+import mongoose, { model, Schema } from 'mongoose';
 import { IOrder } from './order.interface';
 import { ProductModel } from '../product/product.model';
 
 const orderSchema = new Schema<IOrder>({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Email is required'],
     unique: true,
+    validate: {
+      validator: (value) => {
+        const emailRegex =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegex.test(value);
+      },
+      message: '{VALUE}, is not a valid email address',
+    },
   },
   product: {
     type: mongoose.Schema.Types.ObjectId,
